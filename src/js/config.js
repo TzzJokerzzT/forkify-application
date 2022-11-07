5378dcd5b78bfc094b68c3e5a1d6febcf214473f
TzzJokerzzT <alexjesus-4856@hotmail.com>
2022-11-07 00:33:40 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b999a87-1539-4481-ac1e-40f6fcf1d069';
+export const MODAL_CLOSE_SEC = 2.5;
