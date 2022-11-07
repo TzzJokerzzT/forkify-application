c013f7f378de8868e4d62f204a9586fa71e48295
TzzJokerzzT <alexjesus-4856@hotmail.com>
2022-11-07 02:41:07 -0500

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,9 +118,7 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-const newFeatures = function () {
-  console.log('Welcome to the Application!');
-};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,6 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeatures()
 };
 init();
